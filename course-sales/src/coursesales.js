2ed5957acfc2e12a0f7815ea55872db6ea03c6aa
Husnain Naqvi <husnainzoro59@yahoo.com>
2018-08-18 10:18:13 -0400

    Pushing my on going projects

---
+++ course-sales/src/coursesales.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+export default class coursesales extends Component {
+  render() {
+    return (
+    <div>
+     
+       <p> Iam from sales page </p>
+    </div>
+    )
+  }
+}
